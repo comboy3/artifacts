7f381cb899f6c09f992a811912cb649c9b7e8db9
ryo <taba3taba3taba3@gmail.com>
2018-05-10 21:56:44 +0900

    urlsを試しに修正

--- shockhi_check/urls.py
+++ shockhi_check/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('doPost', views.doPost, name='doPost'),
+    path('doPost/', views.doPost, name='doPost'),
 ]

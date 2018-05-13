7e1afca6c9159efe73013e5a3574c4f06c341bc2
ryo <taba3taba3taba3@gmail.com>
2018-05-13 22:08:15 +0900

    トークン変更

--- shockhi_check/service.py
+++ shockhi_check/service.py
@@ -11,7 +11,7 @@ from shockhi_check.models import Shokuhi, User
 logger = logging.getLogger()
 
 #トークン
-access_token = "8GyCMQe6p9BVNXNwvl9ysE2BInxVnvXedqCqiBfUWbkqW1k+/JyjuNMUkP5VcI9YveuboZiu7dzVUJzs/8IIdbeCAEdPkAQQOHpjpuudZwYchZOw4cJWfU5E5xBvmgP3TPMDgzqJzndpm4ERjpFNEQdB04t89/1O/w1cDnyilFU="
+access_token = "48klff7U3aeqw06WBsIOMNonRdMC9N8LenZ1LgQv15t7nO7FW/47je01dXebo+JJsJVN2CAnhZ7Se47kE1Ac+jZZpnPxBQm0nYSfpinQb7nrcQaJ+18xHoqRPUNauicV7Kwkk5iOziBDOXkV8WUvogdB04t89/1O/w1cDnyilFU="
 
 def reply_to_line(params):
 

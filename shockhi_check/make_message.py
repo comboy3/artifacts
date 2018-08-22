5546fab41323301d207c2e7208d7f0d3faeb3fa4
comboy3 <38995035+comboy3@users.noreply.github.com>
2018-08-22 21:38:28 +0900

    Update make_message.py

--- shockhi_check/make_message.py
+++ shockhi_check/make_message.py
@@ -5,7 +5,7 @@ import json
 class LineReplyMessage:
     """ 返信メッセージをJSONデータに編集するクラス """
     # トークン
-    ACCESS_TOKEN = "tyl9iMxaFfz/V3/JH8/V3qVmdJFSMV1HiyD09BNkWu7LJfhQ6u8YxtC8S5KKsd5gPrijBAptKDcCbrUaS+jBS3TCtU6moPNocFfJODAyf6LXeA2o55knBUZ03uEqKJnjzyZV7ET6K0Dj2OkTFe4EkgdB04t89/1O/w1cDnyilFU="
+    ACCESS_TOKEN = "XXXXXXXXX="
     REPLY_ENDPOINT = 'https://api.line.me/v2/bot/message/reply' 
 
     def make_text_response(text):
